65a90d5e8559d28ed97a6c67c35cef10f842f0ec
Cristian Chiru <cristi.chiru@gmail.com>
2025-09-12 18:17:13 +0300

    add render method to avoid writer closure and subtle bugs

--- internal/web/middleware/render.go
+++ internal/web/middleware/render.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gogf/gf/v2/net/ghttp"
 )
 
+// TemplRender renders the given templ.Component to a buffer and writes it to the response
+// This avoids Writer closure by templ, that will cause loss of further response headers and weird bugs, like session cookies not being set.
 func TemplRender(r *ghttp.Request, component templ.Component) error {
 	var buf bytes.Buffer
 	if err := component.Render(r.GetCtx(), &buf); err != nil {
@@ -15,6 +17,7 @@ func TemplRender(r *ghttp.Request, component templ.Component) error {
 		r.Response.WriteStatus(500, "Internal Server Error")
 		return err
 	}
+	r.Response.Header().Set("Content-Type", "text/html; charset=utf-8")
 	r.Response.Write(buf.Bytes())
 	return nil
 }
